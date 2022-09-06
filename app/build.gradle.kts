256d9c637da0de519362875c97cc363a18d605c4
Mehdi HosseinZade <arshen@outlook.com>
2022-09-06 14:48:35 +0430

    move katlin gradle script

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "io.github.xtoolkit.mir"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+        lint.checkReleaseBuilds = false
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("debug") {
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidxCompose.get()
+    }
+
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":util"))
+    implementation(project(":user"))
+    implementation(project(":user:core"))
+    implementation(project(":level"))
+    implementation(project(":level:core"))
+    implementation(project(":playground"))
+    implementation(project(":playground:core"))    
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.collection)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.preview)
+    implementation(libs.androidx.compose.activity)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.android.material)
+    implementation(libs.android.themeadapter)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+    annotationProcessor(libs.room.compiler)
+    kapt(libs.room.compiler)
+}
