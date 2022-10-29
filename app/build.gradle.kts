55ea9530d341ba19699e2325a59c81be0d84876a
Mehdi HosseinZade <arshen@outlook.com>
2022-10-29 22:23:39 +0330

    try ci fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
+import org.jetbrains.kotlin.gradle.internal.Kapt3GradleSubplugin.Companion.isIncludeCompileClasspath
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("com.dicedmelon.gradle.jacoco-android")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,14 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("jacoco")
 }
 
 task("clean") {
     delete(rootProject.buildDir)
 }
 
-allprojects {
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
-}
-
 buildscript {
     dependencies {
-        classpath(libs.test.jacoco)
+        classpath(libs.test.jacoco.android)
     }
 }
